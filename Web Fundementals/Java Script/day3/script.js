4dc28b250952882372b3fe3ad7abf5db81e2246d
Saad Hroub <saad.hroub@gmail.com>
2023-04-14 16:25:37 +0300

    Adding multiple if else and % operator example

--- Web Fundementals/Java Script/day3/script.js
+++ Web Fundementals/Java Script/day3/script.js
@@ -17,5 +17,42 @@ else {
 if the condition is True, we will execute it
 if it is false, it will not be executed
 
-
 */
+
+
+
+
+var grade = 59;
+if(grade>=90){
+    console.log("Excellent");
+}
+else if(grade>=80){
+    console.log("Very good");
+}
+else if(grade>=60){
+    console.log("Pass");
+}
+else{
+    console.log("Hard Luck");
+}
+
+
+var x = 21;
+if(x%2==0){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+
+}
+
+
+
+
+
+
+
+
+
+
+

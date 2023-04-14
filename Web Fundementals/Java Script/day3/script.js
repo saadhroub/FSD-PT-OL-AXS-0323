2bdda64f560212272c0874c1d06d9630b515e522
Saad Hroub <saad.hroub@gmail.com>
2023-04-14 16:17:24 +0300

    adding a conditional example

---
+++ Web Fundementals/Java Script/day3/script.js
@@ -0,0 +1,21 @@
+var isSunny = false;
+var haveMoney = true;
+/* && ==> And      || ==> or
+in && all conditions must be correct
+in || at least one of the conditions must be correct 
+*/ 
+if (isSunny || haveMoney) { 
+    console.log("I will fly to Antalya");
+} 
+else {
+    console.log("I will stay home");
+
+}
+
+/*
+
+if the condition is True, we will execute it
+if it is false, it will not be executed
+
+
+*/

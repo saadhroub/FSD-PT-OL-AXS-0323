d1ecbcc34e62c32dabde00a981fd6db850546397
Saad Hroub <saad.hroub@gmail.com>
2023-05-02 20:06:13 +0300

    doing the lists example

---
+++ Python/fundamentals/types.py
@@ -0,0 +1,21 @@
+name = "Saad"
+age = 19
+is_graduated = False
+
+#Lists: List, Tuple, Set, Dictionaries
+my_list = [1,4,20, "Hi", False, "Apple", age,name]
+fruit = ["Apple", "Orange","Banana","Banana","Banana","Banana","Banana"]
+seasons = ("Spring", "Summer", "Autumn", "Winter")
+
+
+frui_set = {"Apple", "Orange","Banana","Banana"}
+
+fruit.append("Tomato")
+fruit.insert(0,"Saad")
+fruit.sort()
+
+print(len(seasons))
+x = seasons.count("Winter")
+print(x)
+
+

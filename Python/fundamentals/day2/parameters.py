bb1dd279dd832cb5c921ad178b34b9169dd9206f
Saad Hroub <saad.hroub@gmail.com>
2023-05-05 15:28:22 +0300

    ternary operator and named arguments

--- Python/fundamentals/day2/parameters.py
+++ Python/fundamentals/day2/parameters.py
@@ -1,23 +1,32 @@
+# function definition 
+# function implementation
+#math, english, arabic are called parameters 
+def average(math=55,english=55, arabic=55):
+    avg = (math+english+arabic)/3
+    print(f"Math Grade = {math}")
+    print(f"English Grade = {english}")
+    print(f"Arabic Grade = {arabic}")
+    return avg
+
+# montaser_avg = average(95,99)
+# print(f'Montaser average  = {montaser_avg}')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+hamada_avg = average(arabic=80,english=99)
+print(f'Hamada average  = {hamada_avg}')
 
 # in other languages  console.log((stacks>2?"AXSOS":"Not AXSOS")
 # in PYthon [on_true] if [expression] else [on_false]
 
+
+temperature = 35
+if(temperature>=30):
+    print("I will swim")
+
+else:
+    print("I will do hiking")
+
+print('I will swim' if temperature>=30 else 'I will do hiking')
 
 stacks = 5
 if stacks >= 3:
---
+++ Python/fundamentals/day2/test.js
@@ -0,0 +1,10 @@
+temperature = 35;
+if(temperature>=30){
+    console.log("I will swim");
+}
+else{
+    console.log("I will do hiking");
+}
+
+console.log(temperature>=30?"I will swim":"I will do hiking");
+// condition ? true answer : else answer

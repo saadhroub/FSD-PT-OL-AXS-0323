a02c003c22e7fc3310be96b970acdbff3f3d5a45
Saad Hroub <saad.hroub@gmail.com>
2023-07-04 20:34:02 +0300

    java script codes

---
+++ Algo/arrays/introduction.js
@@ -0,0 +1,15 @@
+var student = ["Amani", false, 88.5, 21];
+var students_names = ["Saad", "Shatha", "Ameer", "Ahmad"];
+
+students_names[0] = "Waleed";
+students_names.sort();
+
+console.log(students_names.length)
+
+
+
+
+
+
+
+
---
+++ Algo/arrays/strings.js
@@ -0,0 +1,12 @@
+var st = "Welcome to Jenin";
+
+console.log(st.toUpperCase());
+console.log(st.toLowerCase());
+console.log(st.length);
+console.log(st.charAt(5));
+console.log(st.concat(" Jenin is so great"));
+console.log(st.indexOf("e"))
+console.log(st.lastIndexOf("e"))
+console.log(st.includes("Jenin"))
+console.log(st.includes("Camp"))
+console.log(st.replace("Jenin", "Palestine"))
